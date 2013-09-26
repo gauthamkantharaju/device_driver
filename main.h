a77d883e6f7675c22ef5ca2dc11f80c39e57d1c4
Gautham Kantharaju <gautham.kantharaju@gmail.com>
2013-09-26 09:34:23 +0530

    Create main.h
    Add kernel module header file

---
+++ main.h
@@ -0,0 +1,59 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/debugfs.h>
+#include <linux/spinlock.h>
+#include <linux/wait.h>
+#include <linux/ioctl.h>
+#include <linux/moduleparam.h>
+#include <asm/atomic.h>
+
+#define CONFIG_UACCESS	1
+
+#if (CONFIG_UACCESS == 1)
+#  include <asm/uaccess.h>
+#else
+#  error "Cannot include uaccess"
+#endif
+
+#define dmsg(string, args...) \
+		printk(KERN_DEBUG "%s:%s:%d:" string, MODULE_NAME, __FUNCTION__, __LINE__, ##args)
+
+#define SUCCESS				0
+#define ERROR				-1
+#define MAX_STRLEN			50
+#define MAX_LEN				5
+
+#define MODULE_NAME 		"hello"
+#define MOD_VERSION			"1.0"
+
+#define IOCTL_MAX_CMDS		2
+#define IOCTL_HELLO_MAGIC	'h'
+#define IOCTL_HELLO_TEST1	_IO(IOCTL_HELLO_MAGIC, 0)
+#define IOCTL_HELLO_TEST2	_IO(IOCTL_HELLO_MAGIC, 1)
+
+#if 1
+static inline void iline(char *str)
+{
+	dmsg("String:%s \n", str);
+}
+#endif
+
+typedef struct _context
+{
+	char *buf;
+	int *arry;
+	unsigned int len;
+	unsigned int rindex;
+	unsigned int windex;
+	spinlock_t lock;
+}CNTXT;
+
+#endif /* _MAIN_H_ */
